5bafdf7d80b2af98444b67b5ac9772c39dfe61e2
Ashwin Vijendra <ashwinvijendra@gmail.com>
2022-01-27 19:14:18 +0530

    Meme generator inprogress

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="header">
+      <h1>Meme Generator</h1>
+      <h4>React Course - Project 3</h4>
+    </div>
+  );
+}
+
+export default Header;
