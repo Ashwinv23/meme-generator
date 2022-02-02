d0cd0b7e704c53b26e2ec8f7af6532350d46f996
Ashwin Vijendra <ashwinvijendra@gmail.com>
2022-02-02 13:04:32 +0530

    Added inputs to meme images

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,24 +2,58 @@ import React from "react";
 import memesData from "../memesData";
 
 function Meme() {
-  const [memeImage, setMemeImage] = React.useState("");
+  const [meme, setMeme] = React.useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
+  });
 
   const handleClick = () => {
     const memesArray = memesData.data.memes;
     const randomNumber = Math.floor(Math.random() * memesArray.length);
-    setMemeImage(memesArray[randomNumber].url);
+    const url = memesArray[randomNumber].url;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      randomImage: url,
+    }));
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      [name]: value,
+    }));
   };
 
   return (
     <main>
       <div className="form">
-        <input type="text" placeholder="top" className="form-input" />
-        <input type="text" placeholder="bottom" className="form-input" />
+        <input
+          type="text"
+          placeholder="top"
+          className="form-input"
+          name="topText"
+          value={meme.topText}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          placeholder="bottom"
+          className="form-input"
+          name="bottomText"
+          value={meme.bottomText}
+          onChange={handleChange}
+        />
         <button className="form-button" onClick={handleClick}>
           Get a new meme image
         </button>
       </div>
-      <img src={memeImage} alt="memeimage" className="meme-image" />
+      <div className="meme">
+        <img src={meme.randomImage} alt="memeimage" className="meme-image" />
+        <h2 className="meme-text top">{meme.topText}</h2>
+        <h2 className="meme-text bottom">{meme.bottomText}</h2>
+      </div>
     </main>
   );
 }
